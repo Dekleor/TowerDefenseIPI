c6ed76729b688f3d9c653623f4ae4b600739183f
L.Thibault <laudethibault83@gmail.com>
2022-03-01 17:36:15 +0100

    Ajout du systeme de tir
    + Bullet et + BulletImpact

--- Assets/Turret.cs
+++ Assets/Turret.cs
@@ -1,15 +1,10 @@
-//using System.Threading.Tasks.Dataflow;
-//using System.Collections.Specialized;
-//using System.Numerics;
-//using System.Collections;
-//using System.Collections.Generic;
 using System.Collections;
 using UnityEngine;
 
 public class Turret : MonoBehaviour
 {
-
     public Transform target;
+
     public float range = 15f;
 
     public string enemyTag = "Enemy";
@@ -18,6 +13,13 @@ public class Turret : MonoBehaviour
 
     private float turnSpeed = 6.5f;
 
+    public float fireRate = 1f;
+
+    private float fireCountdown;
+
+    public GameObject bulletPrefab;
+
+    public Transform firePoint;
 
     // Start is called before the first frame update
     void Start()
@@ -25,22 +27,24 @@ public class Turret : MonoBehaviour
         InvokeRepeating("UpdateTarget", 0f, 0.5f);
     }
 
-    void UpdateTarget(){
+    void UpdateTarget()
+    {
         GameObject[] ennemies = GameObject.FindGameObjectsWithTag(enemyTag);
         float shortestDistance = Mathf.Infinity;
         GameObject nearestEnemy = null;
 
         foreach (GameObject enemy in ennemies)
         {
-            float distanceToEnemy = Vector3.Distance(transform.position, enemy.transform.position);
-            if(distanceToEnemy < shortestDistance)
+            float distanceToEnemy =
+                Vector3.Distance(transform.position, enemy.transform.position);
+            if (distanceToEnemy < shortestDistance)
             {
                 shortestDistance = distanceToEnemy;
                 nearestEnemy = enemy;
             }
         }
 
-        if(nearestEnemy != null && shortestDistance <= range)
+        if (nearestEnemy != null && shortestDistance <= range)
         {
             target = nearestEnemy.transform;
         }
@@ -53,19 +57,46 @@ public class Turret : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(target == null)
+        if (target == null)
         {
             return;
         }
 
         Vector3 dir = target.position - transform.position;
         Quaternion lookRotation = Quaternion.LookRotation(dir);
-        Vector3 rotation = Quaternion.Lerp(partToRotate.rotation, lookRotation, Time.deltaTime * turnSpeed).eulerAngles;
+        Vector3 rotation =
+            Quaternion
+                .Lerp(partToRotate.rotation,
+                lookRotation,
+                Time.deltaTime * turnSpeed)
+                .eulerAngles;
         partToRotate.rotation = Quaternion.Euler(0f, rotation.y, 0f);
 
+        if (fireCountdown <= 0f)
+        {
+            Shoot();
+            fireCountdown = 1 / fireRate;
+        }
+
+        fireCountdown -= Time.deltaTime; // chaque on enleve un délai
     }
 
-    private void OnDrawGizmosSelected(){
+    void Shoot() // Fonction de tir
+    {
+        GameObject bulletGO =
+            (GameObject)
+            Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
+        Bullet bullet = bulletGO.GetComponent<Bullet>();
+
+        if (bullet != null)
+        {
+            bullet.Seek (target);
+        }
+        // Debug.Log("Tir effectué");
+    }
+
+    private void OnDrawGizmosSelected()
+    {
         Gizmos.color = Color.red;
         Gizmos.DrawWireSphere(transform.position, range);
     }
